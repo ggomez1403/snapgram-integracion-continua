41f9e646fbe217192b7757965c885e90c1b33a3f
Geraldine Gómez <geralgomez1403@gmail.com>
2023-12-03 15:25:40 -0500

    Sentry implemented

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { App } from "./App";
 import AuthProvider from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://81136f1354082af128925fb050e5a89b@o4506333521838080.ingest.sentry.io/4506333579378688",
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryProvider>
